4270575bb5ebdf8a9a8fedf543fa73ef2be35e01
Matteo Ciapparelli <ciapparellimatteo@gmail.com>
2024-04-02 23:35:00 +0200

    Added feature entities

---
+++ src/FeatureManagement.Database.Abstractions/FeatureManagement/Database/Feature.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Matteo Ciapparelli.
+// Licensed under the MIT license.
+
+namespace FeatureManagement.Database;
+
+/// <summary>
+/// Represents a feature with its associated settings.
+/// </summary>
+public class Feature
+{
+    /// <summary>
+    /// The unique identifier of the feature.
+    /// </summary>
+    public Guid Id { get; }
+
+    /// <summary>
+    /// The unique name of the feature.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The settings associated with the feature.
+    /// </summary>
+    public virtual IEnumerable<FeatureSettings> Settings { get; set; }
+}
+
+// TODO: softdelete + auditing
---
+++ src/FeatureManagement.Database.Abstractions/FeatureManagement/Database/FeatureSettings.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Matteo Ciapparelli.
+// Licensed under the MIT license.
+
+namespace FeatureManagement.Database;
+
+/// <summary>
+/// Represents settings associated with a feature.
+/// </summary>
+public class FeatureSettings
+{
+    /// <summary>
+    /// The unique identifier of the feature settings.
+    /// </summary>
+    public Guid Id { get; }
+
+    /// <summary>
+    /// The filter type used for enabling the feature.
+    /// </summary>
+    public FeatureFilterType FilterType { get; set; }
+
+    /// <summary>
+    /// The parameters associated with the feature settings.
+    /// </summary>
+    public string Parameters { get; set; }
+
+    /// <summary>
+    /// The feature associated with the settings.
+    /// </summary>
+    public virtual Feature Feature { get; set; }
+}
