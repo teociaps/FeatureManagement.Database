48b725438e47d798c7e6aa87cfecf7e08f83bd35
Matteo Ciapparelli <ciapparellimatteo@gmail.com>
2024-05-02 00:55:45 +0200

    Enhance IConfiguration build from JSON string

---
+++ src/FeatureManagement.Database.Abstractions/Microsoft/Extensions/Configuration/JsonStringConfigurationSource.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Matteo Ciapparelli.
+// Licensed under the MIT license.
+
+namespace Microsoft.Extensions.Configuration;
+
+/// <summary>
+/// Represents JSON string as an <see cref="IConfigurationSource"/>.
+/// </summary>
+/// <param name="jsonString">The JSON string to be converted into <see cref="IConfiguration"/>.</param>
+public class JsonStringConfigurationSource(string jsonString) : IConfigurationSource
+{
+    private readonly string _jsonString = jsonString;
+
+    /// <inheritdoc/>
+    public IConfigurationProvider Build(IConfigurationBuilder builder)
+    {
+        return new JsonStringConfigurationProvider(_jsonString);
+    }
+}
