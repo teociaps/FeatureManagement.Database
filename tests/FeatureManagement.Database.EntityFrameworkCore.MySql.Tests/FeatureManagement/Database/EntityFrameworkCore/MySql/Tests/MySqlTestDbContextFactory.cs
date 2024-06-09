3b467e03124292a26d388f7db6c88187c2d13690
Matteo Ciapparelli <ciapparellimatteo@gmail.com>
2024-06-09 22:37:55 +0200

    Added EF Core MySql pkg

---
+++ tests/FeatureManagement.Database.EntityFrameworkCore.MySql.Tests/FeatureManagement/Database/EntityFrameworkCore/MySql/Tests/MySqlTestDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Matteo Ciapparelli.
+// Licensed under the MIT license.
+
+using FeatureManagement.Database;
+using FeatureManagement.Database.EntityFrameworkCore.Tests;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace FeatureManagement.Database.EntityFrameworkCore.MySql.Tests;
+
+/// <summary>
+/// Factory used to build migrations for MySql.
+/// </summary>
+public class MySqlTestDbContextFactory : IDesignTimeDbContextFactory<TestDbContext>
+{
+    public TestDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("FeatureManagement/Database/EntityFrameworkCore/appsettings.json", false)
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<TestDbContext>();
+        var connectionString = configuration.GetConnectionString("MySql");
+
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString),
+            options => options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName));
+
+        return new TestDbContext(optionsBuilder.Options);
+    }
+}
