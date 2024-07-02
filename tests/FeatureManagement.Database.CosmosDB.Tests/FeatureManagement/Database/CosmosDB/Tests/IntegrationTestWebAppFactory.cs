3fb28f7d8b4b638b1ebc65789c7385ee154bf7e9
Matteo Ciapparelli <ciapparellimatteo@gmail.com>
2024-07-03 01:11:22 +0200

    Added CosmosDB tests

---
+++ tests/FeatureManagement.Database.CosmosDB.Tests/FeatureManagement/Database/CosmosDB/Tests/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Matteo Ciapparelli.
+// Licensed under the MIT license.
+
+using DotNet.Testcontainers.Builders;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Testcontainers.CosmosDb;
+
+namespace FeatureManagement.Database.CosmosDB.Tests;
+
+public sealed class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly CosmosDbContainer _cosmosDbContainer = new CosmosDbBuilder()
+            .WithEnvironment("AZURE_COSMOS_EMULATOR_PARTITION_COUNT", "2")
+            .WithEnvironment("AZURE_COSMOS_EMULATOR_ENABLE_DATA_PERSISTENCE", "true")
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(CosmosDbBuilder.CosmosDbPort))
+            .Build();
+
+    internal string ConnectionString => _cosmosDbContainer.GetConnectionString();
+
+    public async Task InitializeAsync()
+    {
+        await _cosmosDbContainer.StartAsync();
+
+        using var scope = Services.CreateScope();
+        var connectionFactory = scope.ServiceProvider.GetRequiredService<ICosmosDBConnectionFactory>();
+        var cosmosDbOptions = scope.ServiceProvider.GetRequiredService<IOptions<CosmosDBOptions>>();
+
+        await Seed.SeedDataAsync(cosmosDbOptions.Value, connectionFactory);
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _cosmosDbContainer.DisposeAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(ConfigureServices);
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDatabaseFeatureManagement<FeatureStore>()
+            .UseCosmosDB(options =>
+            {
+                options.EndpointUri = $"https://localhost:{CosmosDbBuilder.CosmosDbPort}";
+                options.AccountKey = CosmosDbBuilder.DefaultAccountKey;
+                options.DatabaseName = "TestDatabase";
+            });
+    }
+}
---
+++ tests/FeatureManagement.Database.CosmosDB.Tests/FeatureManagement/Database/CosmosDB/Tests/Seed.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Matteo Ciapparelli.
+// Licensed under the MIT license.
+
+using Microsoft.Azure.Cosmos;
+
+namespace FeatureManagement.Database.CosmosDB.Tests;
+
+using static FeatureManagement.Database.Features;
+
+internal static class Seed
+{
+    internal static async Task SeedDataAsync(CosmosDBOptions cosmosDBOptions, ICosmosDBConnectionFactory cosmosDBConnectionFactory)
+    {
+        var client = cosmosDBConnectionFactory.CreateClient();
+        var database = await client.CreateDatabaseIfNotExistsAsync(cosmosDBOptions.DatabaseName);
+
+        await database.Database.CreateContainerIfNotExistsAsync(cosmosDBOptions.FeaturesCollectionName, "/Name");
+
+        if (cosmosDBOptions.UseSeparateContainers)
+            await database.Database.CreateContainerIfNotExistsAsync(cosmosDBOptions.FeatureSettingsCollectionName, "/FeatureId");
+
+        var featuresContainer = cosmosDBConnectionFactory.GetFeaturesContainer();
+        var featureSettingsContainer = cosmosDBConnectionFactory.GetFeatureSettingsContainer();
+
+        List<Feature> features =
+        [
+            new()
+            {
+                Id = Guid.Parse("7C81E846-DC77-4AFF-BF03-8DD8BB2D3194"),
+                Name = FirstFeature,
+                RequirementType = Microsoft.FeatureManagement.RequirementType.All,
+            },
+            new()
+            {
+                Id = Guid.Parse("D3C82992-2F12-4008-9376-DA37695A2747"),
+                Name = SecondFeature,
+                RequirementType = Microsoft.FeatureManagement.RequirementType.All,
+            }
+        ];
+
+        List<FeatureSettings> settings =
+        [
+            new()
+            {
+                Id = Guid.Parse("672DC1BD-9C5B-44CE-8461-234B262A8395"),
+                FeatureId = features[0].Id,
+                FilterType = FeatureFilterType.TimeWindow,
+                Parameters = """{"Start": "Mon, 01 May 2023 13:59:59 GMT", "End": "Sat, 01 July 2023 00:00:00 GMT"}"""
+            }
+        ];
+
+        await featuresContainer.CreateItemAsync(features[0], new PartitionKey(features[0].Name));
+        await featuresContainer.CreateItemAsync(features[1], new PartitionKey(features[0].Name));
+
+        await featureSettingsContainer.CreateItemAsync(settings[0], new PartitionKey(settings[0].FeatureId.ToString()));
+    }
+}
